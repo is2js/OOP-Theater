0fac692fcbe0c7257d0629f8af1ec01d5e2972a1
is2js <tingstyle1@gmail.com>
2022-06-20 23:10:09 +0900

    feat(Ticket): 발행주체로부터 정보를 받아올 때 객체(식별자)로 받아오고, 정보제공시에는 객체내부의 정보를 포인터의 포인터로 내어준다.

--- src/main/java/theater/Ticket.java
+++ src/main/java/theater/Ticket.java
@@ -1,11 +1,19 @@
 package theater;
 
 public class Ticket {
+    private final Theater theater;
+
+    // 발행되는 객체는, 발행주체를 생성할 때부터 알고 있으며, 발급주체가 불변하도록 final field로 박아둔다.
     public Ticket(final Theater theater) {
-        throw new UnsupportedOperationException("Ticket#Ticket not write.");
+        this.theater = theater;
     }
 
     public boolean isValid(final Theater theater) {
         throw new UnsupportedOperationException("Ticket#isValid not write.");
+    }
+
+    // 정보를 값이 아닌 식별자인 객체로 받아와 저장해놨다면, 정보를 내어줄 때 포인터의 포인터로 내어준다.
+    public Long getFee() {
+        return theater.getFee();
     }
 }

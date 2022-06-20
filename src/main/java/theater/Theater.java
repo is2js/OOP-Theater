8e7cbef23893ca34dd58625ab198c6ebfc78215c
is2js <tingstyle1@gmail.com>
2022-06-20 21:48:35 +0900

    feat(Theater): Ticket발급 주체로서 Ticket의 기본정보를 가지고 있으며, 제공해준다.

---
+++ src/main/java/theater/Theater.java
@@ -0,0 +1,14 @@
+package theater;
+
+public class Theater {
+
+    private final Long fee;
+
+    public Theater(final Long fee) {
+        this.fee = fee;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+}

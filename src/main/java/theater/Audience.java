033b46572fa4061abd1cdf6c49acd87866f0e3bc
is2js <tingstyle1@gmail.com>
2022-06-21 11:07:13 +0900

    feat(Audience): 거래를 통해 받아오는 재료객체(Ticket)를 내부 상태로 저장(setter)해야해서 안다.
     - 누군가에게 반환해주는 주능기능getter가 아니라 setter for 내 상태라면 void 기능이다.

--- src/main/java/theater/Audience.java
+++ src/main/java/theater/Audience.java
@@ -1,6 +1,23 @@
 package theater;
 
 public class Audience {
+    private Ticket ticket = Ticket.EMPTY;
+    private Invitation invitation = Invitation.EMPTY;
+    private final Long amount;
+
+    public Audience(final Long amount) {
+        this.amount = amount;
+    }
+
+    // 고객의 사는 기능은 파는놈을 인자로 받아서 물건을 받아 -> 상태값으로 저장한다.
+    // -> 파는 놈의 파는 기능을 이용하며, 가져온 물건은 상태값으로 저장하여 void 메서드다.
+    // -> 소모성 재료객체를 상태field(not final)로 가지고 있는다면,  default 값은 null객체로 상태값을 준다.
+    public void buyTicket(TicketSeller ticketSeller){
+        ticket = ticketSeller.getTicket(this);
+    }
+
+
+
     public void setInvitation(final Invitation invitation) {
         throw new UnsupportedOperationException("Audience#setInvitation not write.");
     }
--- src/main/java/theater/TicketOffice.java
+++ src/main/java/theater/TicketOffice.java
@@ -12,15 +12,11 @@ public class TicketOffice {
         this.amount = amount;
     }
 
-    // 외부가 알아서, 받는 기능.(Theater -> Ticketoffice)
-    // - 1:N으로 아는 상황이니, offices들을 돌면서 받아감.
     public void addTicket(final Ticket ticket) {
         this.tickets.add(ticket);
     }
 
-    // 외부가 알아서 빼가지만, 빼가기 전에 먼저, 객체 정보를 조회하는 기능도 제공해준다.
     public Long getTicketPrice() {
-        // 소모성 재료객체는 수량을 확인한다.
         if (tickets.size() == 0) {
             return 0L;
         }
@@ -29,23 +25,17 @@ public class TicketOffice {
         }
     }
 
-    // 외부가 알아서, 빼가는 기능
-    //1) 일반 audience를 만난 seller가 창고에서 빼감
     public Ticket getTicketWithFee() {
-        // 1) 창고에서 빼갈 때, 소모되어 수량제한이 있는 재료객체는, 갯수가 0이면 null객체를 반환한다.
         if (tickets.size() == 0) {
             return Ticket.EMPTY;
         }
-        // 2) 창고에서 빼가는 상황일 때, 빼간 만큼 1개씩 remove 와 그 판매비용을 챙긴다.
-        //
+
         final Ticket ticket = tickets.remove(0);
         amount += ticket.getFee();
         return ticket;
     }
 
-    //2) invitation을 가진 audience를 만난 seller가 창고에서 빼감
     public Ticket getTicketWithNoFee() {
-        // 1) 창고에서 빼갈 때, 소모되는 것은 갯수 체크후 0개면 null객체 반환
         if (tickets.size() == 0) {
             return Ticket.EMPTY;
         }

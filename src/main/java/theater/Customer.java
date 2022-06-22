df35c6ae037307f2deb2200bd3968a90a889b55f
is2js <tingstyle1@gmail.com>
2022-06-22 23:37:50 +0900

    feat(Customer): 데이터객체(Reservation)은 차후 받는다면, not final & NULL 데이터객체로 초기화해놓자.

---
+++ src/main/java/theater/Customer.java
@@ -0,0 +1,12 @@
+package theater;
+
+public class Customer {
+
+    // 데이터 객체를, 태어난 이후 받는다면(구매), 미리 NULL객체로 초기화해서 상대방 확인시, NULL객체로 확인하게 한다.
+    public Reservation reservation = Reservation.NONE;
+    private Money amount; // VO필드는 재할당이 운명이라 not final이다.
+
+    public Customer(final Money amount) {
+        this.amount = amount;
+    }
+}

732f9e33cb62f53994b9485cf288cae7bd6ea846
is2js <tingstyle1@gmail.com>
2022-06-22 14:33:21 +0900

    feat(AmountDiscount): DiscountPolicy 정책 중 1개로 먼저 추상화를 하면서, 액션메서드에 내용을 구현하기 위해 DiscountCondition도 같이 구현한다. 2번째 정책조건의 구상형(Sequence, Time)들이 적용된 최종구상체들의 추상체가 될 것이다.

---
+++ src/main/java/theater/discount/AmountDiscount.java
@@ -0,0 +1,17 @@
+package theater.discount;
+
+import theater.Money;
+import theater.discount.DiscountPolicy.AMOUNT;
+
+public abstract class AmountDiscount implements AMOUNT, DiscountCondition{
+    private Money amount;
+
+    protected AmountDiscount(final Money amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public Money calculateFee(final Money fee) {
+        return fee.minus(amount)
+    }
+}

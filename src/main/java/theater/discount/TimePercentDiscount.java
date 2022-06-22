51f8e2bc7973eac964661cfa4f1629225ca0f731
is2js <tingstyle1@gmail.com>
2022-06-22 14:52:23 +0900

    feat(TimePercentDiscount): PercentDiscount를 추상체로보고, 정책조건의 발동조건 메서드를 time기준으로 구현한 최종 구상체

---
+++ src/main/java/theater/discount/TimePercentDiscount.java
@@ -0,0 +1,18 @@
+package theater.discount;
+
+import java.time.LocalDateTime;
+import theater.Screening;
+
+public class TimePercentDiscount extends PercentDiscount {
+    private LocalDateTime whenScreened;
+
+    protected TimePercentDiscount(final double percent, final LocalDateTime whenScreened) {
+        super(percent);
+        this.whenScreened = whenScreened;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(final Screening screening, final int audienceCount) {
+        return screening.whenScreened == whenScreened;
+    }
+}

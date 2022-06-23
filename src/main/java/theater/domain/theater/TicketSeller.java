b37636905a3c7ab7d7fbf41298bafabd555fb450
is2js <tingstyle1@gmail.com>
2022-06-24 00:26:12 +0900

    feat(TicketSeller): office로 전달될 인자를 내부에서 사용해 의존성이 추가되었다. 디미터법칙에 의해 1개 의존성은 1곳에서만 쓰이도록 -> 내부 movie 기능호출을 ticketOffice에게 책임 위임한다. -> 이렇게 직접의존성을가진객체가 대신 기능해주는 것을 wrapping method라 함.

--- src/main/java/theater/domain/theater/TicketSeller.java
+++ src/main/java/theater/domain/theater/TicketSeller.java
@@ -14,27 +14,25 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    // 고객이 예매권을 사기 위해, 갑의 거래기능을 이용
-    public Reservation reserve(Customer customer, Theater theater, Movie movie, Screening screening, int count) {
-        //0) 거래는 중첩if를 만족할때만 성공(특수) & (그외 대부분의 코드분기) 중첩된 if의 else마다 매번 실패값을 return해야하므로
-        // -> 애초에 <실패값인 null객체를 반환변수에 미리 할당>해놓고 -> <중첩if의 성공시만 바뀌는, else없는 & earlyReturn없는 코드>를 짠다.
-        // --> my) else없는 중첩if문: 미리 맨 위에 else에 해당하는 값이 세팅되어있어야 한다.
+    public Reservation reserve(Customer customer,
+                               Theater theater,
+                               Movie movie,
+                               Screening screening,
+                               int count) {
         Reservation reservation = Reservation.NONE;
-        //1) 물건가격을 받아온다.
-        final Money price = movie.calculateFee(screening, count);
-        //2) A: 사는놈이 돈있는지 검증해야한다.
+        // 전달되는 의존성을 중간에서 사용하면, 디미터 원칙위반 -> 여러 사용처에서 변화에 타격
+        // -> 이미 넘어가는 의존성을 직접 사용하지말고, 1곳에서 사용하도록 책임을 위임하고, 결과만 받자.
+        //final Money price = movie.calculateFee(screening, count);
+        // --> 결국에 전달 받을 거, 미리 전달해서 사용한다.
+        // ---> 이렇게 의존성과 직접적인 관계의 객체가 대신 호출해주는 것을 wrapping method라 한다.
+        final Money price = ticketOffice.calculateFee(movie, screening, count);
+
         if (customer.hasAmount(price)) {
-            //3-1) 돈 있을 경우, 물건을 받아온다. (돈 없는 경우, if에 안걸리고 그대로 NULL물건을 반환)
-            // -> 받아온 물건은 NULL물건일 수도 있다.
             reservation = ticketOffice.reserve(theater, movie, screening, count);
-            //3-2) B: 물건이 정상적이어야, 사는놈의 돈을 차감한다.
             if (reservation != Reservation.NONE) {
-                //4) (돈있고, 물건도 정상) A & B라면, 받아온 고객의 돈을 차감하고, 받아온 물건을 반환한다.
                 customer.minusAmount(price);
             }
         }
-        //5) 중첩if에 걸려 성공했다면, 사는놈 돈 차감 & NULL아닌 물건을 반환한다.
-        //   -> 하나라도 if에 안걸렸다면(else)했다면, 미리 세팅해놓은 default else용 실패값 NULL객체가 반환된다.
         return reservation;
     }
 }

4028292bfef62f48c8679b822807031a9fc38a37
is2js <tingstyle1@gmail.com>
2022-06-24 00:37:32 +0900

    feat(TicketOffice): seller에게 직접의존성을 가진 office가 wrapping method를 제공하여, 메서드 추가 -> 내부 기존코드와 중복 확인 -> 대체

--- src/main/java/theater/domain/theater/TicketSeller.java
+++ src/main/java/theater/domain/theater/TicketSeller.java
@@ -20,11 +20,6 @@ public class TicketSeller {
                                Screening screening,
                                int count) {
         Reservation reservation = Reservation.NONE;
-        // 전달되는 의존성을 중간에서 사용하면, 디미터 원칙위반 -> 여러 사용처에서 변화에 타격
-        // -> 이미 넘어가는 의존성을 직접 사용하지말고, 1곳에서 사용하도록 책임을 위임하고, 결과만 받자.
-        //final Money price = movie.calculateFee(screening, count);
-        // --> 결국에 전달 받을 거, 미리 전달해서 사용한다.
-        // ---> 이렇게 의존성과 직접적인 관계의 객체가 대신 호출해주는 것을 wrapping method라 한다.
         final Money price = ticketOffice.calculateFee(movie, screening, count);
 
         if (customer.hasAmount(price)) {

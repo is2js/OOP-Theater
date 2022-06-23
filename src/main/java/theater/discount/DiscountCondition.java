8e038431a80779232c832b712d8b7b0b60d5920a
is2js <tingstyle1@gmail.com>
2022-06-23 23:53:32 +0900

    feat(DiscountCondition): 기존에 가지던 action메서드는 DiscountPolicy에게 위임하고 1개의 메서드를 갖는 인터페이스가 되었다. 전략패턴

--- src/main/java/theater/discount/DiscountCondition.java
+++ src/main/java/theater/discount/DiscountCondition.java
@@ -1,9 +1,7 @@
 package theater.discount;
 
-import theater.domain.Money;
 import theater.domain.Screening;
 
 public interface DiscountCondition {
     public boolean isSatisfiedBy(Screening screening, int audienceCount);
-    public Money calculateFee(Money fee);
 }

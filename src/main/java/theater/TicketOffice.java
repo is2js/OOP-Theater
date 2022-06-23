ece75e5533a671b535e53c1a9e3a7135672685b6
is2js <tingstyle1@gmail.com>
2022-06-23 12:25:20 +0900

    feat(TicketOffice): 계약은 서로 목록을 받아 저장하는데, office는 갑으로서 거래조건과 함께 받아 저장하며, 갑으로서 먼저 계약/계약취소 t/f여부를 을에게 boolean으로 전달해준다.

--- src/main/java/theater/TicketOffice.java
+++ src/main/java/theater/TicketOffice.java
@@ -1,17 +1,34 @@
 package theater;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TicketOffice {
 
     private Money amount;
+    private final Map<Theater, Double> commissionRate = new HashMap<>();
     public TicketOffice(Money amount) {
         this.amount = amount;
     }
 
+    // 계약: 거래theater + 거래조건 저장하기
+    // -> 내부에서 처리되며, 성공/실패를 boolean으로 알려주어, 갑 저장성공시 -> 을 저장하는 순서로 하게 한다.
     public boolean contract(final Theater theater, final Double rate) {
-        throw new UnsupportedOperationException("TicketOffice#contract not write.");
-    }
+        // 저장할 땐, 상위개념 중복여부부터 검사한다.
+        if (commissionRate.containsKey(theater)) {
+            return false;
+        }
 
-    public boolean cancel(final Theater theater) {
-        throw new UnsupportedOperationException("TicketOffice#cancel not write.");
+        commissionRate.put(theater, rate);
+        return true;
+    }
+    // 계약취소 -> 삭제개념으로 존재여부 검사 + 내부처리로서 t/f반환
+    // -> 계약취소도 상대방간 이루어져야하는데, 갑(office)부터 remove하고 t/f를 받아 -> 을(theater)가 remove한다.
+    public boolean cancel(Theater theater) {
+        if (!commissionRate.containsKey(theater)) {
+            return false;
+        }
+        commissionRate.remove(theater);
+        return true;
     }
 }

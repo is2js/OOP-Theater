e1318efd520b8eccc9ada626d07b5b2b4461105c
is2js <tingstyle1@gmail.com>
2022-06-23 12:09:59 +0900

    feat(TicketOffice): 발행주체인 Theater에게 창고로서 데이터객체를 받아오는 책임 제거

--- src/main/java/theater/TicketOffice.java
+++ src/main/java/theater/TicketOffice.java
@@ -1,46 +1,10 @@
 package theater;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TicketOffice {
 
     private Money amount;
-    private List<Ticket> tickets = new ArrayList<>();
-
     public TicketOffice(Money amount) {
         this.amount = amount;
-    }
-
-    public void addTicket(final Ticket ticket) {
-        this.tickets.add(ticket);
-    }
-
-    public Long getTicketPrice() {
-        if (tickets.size() == 0) {
-            return 0L;
-        }
-        else {
-            return tickets.get(0).getFee();
-        }
-    }
-
-    public Ticket getTicketWithFee() {
-        if (tickets.size() == 0) {
-            return Ticket.EMPTY;
-        }
-
-        final Ticket ticket = tickets.remove(0);
-        amount += ticket.getFee();
-        return ticket;
-    }
-
-    public Ticket getTicketWithNoFee() {
-        if (tickets.size() == 0) {
-            return Ticket.EMPTY;
-        }
-        final Ticket ticket = tickets.remove(0);
-        return ticket;
     }
 
     public boolean contract(final Theater theater, final Double rate) {

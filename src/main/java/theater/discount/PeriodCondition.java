412bf0c63fcb09f41a9d7427f1a07955546fa8dd
is2js <tingstyle1@gmail.com>
2022-06-23 23:59:27 +0900

    feat(DiscountCondition): 전략객체 2개(SequenceCondition,PeriodCondition)  구상화

---
+++ src/main/java/theater/discount/PeriodCondition.java
@@ -0,0 +1,17 @@
+package theater.discount;
+
+import java.time.LocalDateTime;
+import theater.domain.Screening;
+
+public class PeriodCondition implements DiscountCondition {
+    private LocalDateTime whenScreened;
+
+    public PeriodCondition(final LocalDateTime whenScreened) {
+        this.whenScreened = whenScreened;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(final Screening screening, final int audienceCount) {
+        return screening.whenScreened.equals(whenScreened);
+    }
+}
---
+++ src/main/java/theater/discount/SequenceCondition.java
@@ -0,0 +1,16 @@
+package theater.discount;
+
+import theater.domain.Screening;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(final int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(final Screening screening, final int audienceCount) {
+        return screening.sequence == sequence;
+    }
+}

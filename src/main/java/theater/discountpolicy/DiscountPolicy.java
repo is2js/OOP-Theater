305f5ed320387f899fabfe07bcba1339a7683a82
is2js <tingstyle1@gmail.com>
2022-06-26 23:11:42 +0900

    feat(DiscountPolicy): 추상체변수의 getter후 로직을 추상체 내부로 위임한다면 default메서드로 넘어가며, 내부에서는 전략메서드를 내수용으로서 편하게 사용하여 정의한다.

--- src/main/java/theater/discountpolicy/DiscountPolicy.java
+++ src/main/java/theater/discountpolicy/DiscountPolicy.java
@@ -1,6 +1,5 @@
 package theater.discountpolicy;
 
-import theater.discountpolicy.factory.condition.DiscountCondition;
 import theater.discountpolicy.factory.PolicyFactory;
 import theater.domain.Money;
 import theater.domain.Screening;
@@ -14,11 +13,6 @@ public class DiscountPolicy {
     }
 
     public Money calculateFee(Screening screening, int count, Money fee){
-        for (final DiscountCondition condition : factory.getConditions()) {
-            if (condition.isSatisfiedBy(screening, count)) {
-                return factory.calculateFee(fee);
-            }
-        }
-        return fee;
+        return factory.calculateFee(fee);
     }
 }
--- src/main/java/theater/discountpolicy/factory/PolicyFactory.java
+++ src/main/java/theater/discountpolicy/factory/PolicyFactory.java
@@ -3,7 +3,19 @@ package theater.discountpolicy.factory;
 import java.util.Set;
 import theater.discountpolicy.factory.condition.DiscountCondition;
 import theater.discountpolicy.strategy.Calculator;
+import theater.domain.Money;
+import theater.domain.Screening;
 
 public interface PolicyFactory extends Calculator {
+
     Set<DiscountCondition> getConditions();
+
+    default Money calculateFee(Screening screening, int count, Money fee){
+        for (final DiscountCondition condition : getConditions()) {
+            if (condition.isSatisfiedBy(screening, count)) {
+                return calculateFee(fee);
+            }
+        }
+        return fee;
+    }
 }
